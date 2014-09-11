3a26f2be60a265aa00e599386116a6ab0b790657
Ron Ellis <rone@benetech.org>
2014-09-11 10:14:25 -0700

    perfect our replication cronjob

--- recipes/replication_cronjob.rb
+++ recipes/replication_cronjob.rb
@@ -2,7 +2,7 @@ cron_d 'replicate-live-solr-index' do
   minute  0
   hour    0
   day     6
-  command "curl http://solr1.#{node.chef_environment}.bookshare.org:8080/core0/replication?command=fetchindex"
+  command "curl -s http://localhost:8080/core0/replication?command=fetchindex"
   user    'root'
   mailto  'sysadmin@bookshare.org'
   path    '/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bookshare/sbin:/usr/local/bookshare/bin:/usr/local/bin'
