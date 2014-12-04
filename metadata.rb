7621b3bc7d10890465e8f53216d0790519eb2799
Ron Ellis <rone@benetech.org>
2014-12-04 14:23:17 -0800

    updated schema

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'rone@benetech.org'
 license          'All rights reserved'
 description      'Installs/Configures Apache Solr'
 long_description 'Installs/Configures Apache Solr'
-version          '0.1.23'
+version          '0.1.24'
 
 depends 'java', '~> 1.28.0'
